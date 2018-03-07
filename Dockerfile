58ea94e1b389033abcaae7082af8b2cd62db082e
GavinHanson <gavin@cylo.io>
2018-03-07 20:04:30 +0000

    Fully Working.

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM ae9a7bf53c48
+FROM repo.cylo.io/alpine-mongo:latest
 
 LABEL maintainer="Gavin Hanson <glow@cylo.io>"
 
@@ -10,6 +10,8 @@ RUN apk update && apk add nodejs \
 ADD entrypoint.sh /entrypoint.sh
 RUN chmod 755 /*.sh
 RUN chmod +x /*.sh
+
+ADD configs/settings.yml /settings.yml
 
 
 CMD ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,26 @@
 #!/bin/sh
 /bin/sh /configmongo.sh
 
-echo "Installing Lets Chat"
-cd /
-git clone https://github.com/sdelements/lets-chat.git
-cd /lets-chat
-npm install
+
+if [ ! -f /etc/letschatinstalled ]; then
+    echo "Installing Lets Chat"
+    cd /data
+    git clone https://github.com/sdelements/lets-chat.git
+    cd /data/lets-chat
+    npm install
+
+    mkdir -p /data/uploads
+    mv /settings.yml /data/lets-chat/settings.yml
+
+    sed -i "s/DB_USER/${DB_USER}/g" /data/lets-chat/settings.yml
+    sed -i "s/DB_PASS/${DB_PASS}/g" /data/lets-chat/settings.yml
+
+    touch /etc/letschatinstalled
+elseif
+    cd /data/lets-chat
+fi
+
+npm start &
 
 echo "Starting MongoDB with Supervisor"
 exec /usr/bin/supervisord -n -c /etc/supervisord.conf
