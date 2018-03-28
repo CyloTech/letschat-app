70d0a09f10fb376dbca9b55adf1af526bb170dc7
GavinHanson <gavin@cylo.io>
2018-03-28 13:11:25 +0100

    Moved npm start to supervisor.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
+set -x
 /bin/sh /configmongo.sh
 
-
-if [ ! -f /etc/letschatinstalled ]; then
+if [ ! -f /etc/app_configured ]; then
     echo "Installing Lets Chat"
     cd /data
     git clone https://github.com/sdelements/lets-chat.git
@@ -15,12 +15,32 @@ if [ ! -f /etc/letschatinstalled ]; then
     sed -i "s/DB_USER/${DB_USER}/g" /data/lets-chat/settings.yml
     sed -i "s/DB_PASS/${DB_PASS}/g" /data/lets-chat/settings.yml
 
-    touch /etc/letschatinstalled
+cat << EOF >> /etc/supervisor/conf.d/letschat.conf
+[program:letschat]
+directory=/data/lets-chat
+command=npm start
+autostart=true
+autorestart=true
+priority=50
+stdout_events_enabled=true
+stderr_events_enabled=true
+stdout_logfile=/dev/stdout
+stdout_logfile_maxbytes=0
+stderr_logfile=/dev/stderr
+stderr_logfile_maxbytes=0
+EOF
+
+    touch /etc/app_configured
+
+    #Tell Apex we're done installing.
+    curl -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST "https://api.cylo.io/v1/apps/installed/$INSTANCE_ID"
 elseif
     cd /data/lets-chat
 fi
 
-npm start &
+echo "Sleeping for 10 seconds to give time for the database to come online"
+sleep 10
+#npm start &
 
 echo "Starting MongoDB with Supervisor"
 exec /usr/bin/supervisord -n -c /etc/supervisord.conf
